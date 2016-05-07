dda742487ba8b4614311ea4af0f3fe27445a2090
AnchorRen <543177205@qq.com>
2016-05-07 19:47:53 +0800

    a search result class,include date items、pages、count etc...

---
+++ src/main/java/com/lmars/search/pojo/SearchResult.java
@@ -0,0 +1,44 @@
+package com.lmars.search.pojo;
+
+import java.util.List;
+
+public class SearchResult {
+
+	private List<SearchItem> itemList;
+	private long recordCount;
+	private int pageCount;
+	private int curPage;
+
+	public List<SearchItem> getItemList() {
+		return itemList;
+	}
+
+	public void setItemList(List<SearchItem> itemList) {
+		this.itemList = itemList;
+	}
+
+	public long getRecordCount() {
+		return recordCount;
+	}
+
+	public void setRecordCount(long recordCount) {
+		this.recordCount = recordCount;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public int getCurPage() {
+		return curPage;
+	}
+
+	public void setCurPage(int curPage) {
+		this.curPage = curPage;
+	}
+
+}
