f1c4a0f5953dcf6f737594a00bbf83973f49752c
AnchorRen <543177205@qq.com>
2016-05-07 19:46:17 +0800

    Search Item include WMS layer and MapService layer info

---
+++ src/main/java/com/lmars/search/pojo/SearchItem.java
@@ -0,0 +1,51 @@
+package com.lmars.search.pojo;
+
+public class SearchItem {
+
+	private String id;
+	private String serviceId;
+	private String mapName;
+	private String mapDesc;
+	private String images;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getServiceId() {
+		return serviceId;
+	}
+
+	public void setServiceId(String serviceId) {
+		this.serviceId = serviceId;
+	}
+
+	public String getMapName() {
+		return mapName;
+	}
+
+	public void setMapName(String mapName) {
+		this.mapName = mapName;
+	}
+
+	public String getMapDesc() {
+		return mapDesc;
+	}
+
+	public void setMapDesc(String mapDesc) {
+		this.mapDesc = mapDesc;
+	}
+
+	public String getImages() {
+		return images;
+	}
+
+	public void setImages(String images) {
+		this.images = images;
+	}
+
+}
